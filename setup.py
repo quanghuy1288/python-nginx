542b78d7bbd02a5635f73c6e79a545140b95f504
HuyVQ <huyquv@gmail.com>
2021-03-10 22:29:31 +0700

    update for quanghuy1288

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup
 
 setup(
-    name='python-nginx',
+    name='python-nginx-fm',
     version='1.5.4',
     description='Create and modify nginx serverblock configs in Python',
     author='Jacob Cook',
     author_email='jacob@peakwinter.net',
-    url='https://github.com/peakwinter/python-nginx',
+    url='https://github.com/quanghuy1288/python-nginx',
     py_modules=['nginx'],
     keywords=['nginx', 'web servers', 'serverblock', 'server block'],
-    download_url='https://github.com/peakwinter/python-nginx/archive/1.5.4.zip',
+    download_url='https://github.com/quanghuy1288/python-nginx/archive/1.5.4.zip',
     license='GPLv3',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
